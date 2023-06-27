d31b3573165a78fdac47f8972fb41bd3f7eee819
Swagpillar <kfeeley20@gmail.com>
2023-06-27 11:44:44 +0100

    tempraroay changlers

--- main.lua
+++ main.lua
@@ -1,8 +1,5 @@
 REWORKEDITEMS = RegisterMod("Reworked Items", 1)
 
-local hiddenItemManager = require("ri_scripts.hiddenitemmanager")
-hiddenItemManager:Init(REWORKEDITEMS)
-
 REWORKEDITEMS.Enums = require("ri_scripts.Enums")
 REWORKEDITEMS.Helpers = require("ri_scripts.Helpers")
 
