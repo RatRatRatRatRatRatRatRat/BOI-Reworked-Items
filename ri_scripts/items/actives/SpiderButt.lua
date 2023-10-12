4997fba393496fac53d1d8504e38c05fdd286674
Swagpillar <kfeeley20@gmail.com>
2023-10-12 20:11:22 +0100

    big ass spider ass

--- ri_scripts/items/actives/SpiderButt.lua
+++ ri_scripts/items/actives/SpiderButt.lua
@@ -2,9 +2,11 @@ local manager = Game():GetPlayerManager()
 
 REWORKEDITEMS:AddCallback(ModCallbacks.MC_POST_UPDATE, function(_)
     if not manager:AnyoneHasCollectible(CollectibleType.COLLECTIBLE_SPIDER_BUTT) then return end
+    local frames = (math.sin(Game():GetFrameCount() * 8) + 1) * 0.15 + 0.1
+
     for _, entity in ipairs(Isaac.GetRoomEntities()) do
         if entity.HitPoints <= 10 and entity:IsActiveEnemy() and not entity:IsInvincible() then
-            entity:SetColor(Color(1, 1, 1, 1, 0.3, 0.3, 0.3), 1, 1, true, true)
+            entity:SetColor(Color(1, 1, 1, 1, frames, frames, frames), 1, 1, true, true)
         end
     end
 end)
