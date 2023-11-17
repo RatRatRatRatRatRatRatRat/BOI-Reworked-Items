3f37a04926cffa69bcb5421aa1e63c15f1ddbbd0
kittenchilly <mario.pikmin.zelda@gmail.com>
2023-11-17 17:35:13 -0600

    Use MC_POST_FAMILIAR_FIRE_PROJECTILE where able

--- ri_scripts/items/passives/GhostBaby.lua
+++ ri_scripts/items/passives/GhostBaby.lua
@@ -1,7 +1,5 @@
 ---@param tear EntityTear
-REWORKEDITEMS:AddCallback(ModCallbacks.MC_POST_FAMILIAR_FIRE_PROJECTILE, function(_, tear, familiar)
-    if familiar and familiar.Variant == FamiliarVariant.GHOST_BABY then
-        tear:AddTearFlags(TearFlags.TEAR_PIERCING)
-        tear:ChangeVariant(TearVariant.CUPID_BLUE)
-    end
-end)
+REWORKEDITEMS:AddCallback(ModCallbacks.MC_POST_FAMILIAR_FIRE_PROJECTILE, function(_, tear)
+    tear:AddTearFlags(TearFlags.TEAR_PIERCING)
+    tear:ChangeVariant(TearVariant.CUPID_BLUE)
+end, FamiliarVariant.GHOST_BABY)
--- ri_scripts/items/passives/LilLoki.lua
+++ ri_scripts/items/passives/LilLoki.lua
@@ -4,12 +4,12 @@ REWORKEDITEMS:AddCallback(ModCallbacks.MC_FAMILIAR_UPDATE, function(_, familiar)
     local sprite = familiar:GetSprite()
     if data.IsCardinal == nil then
         data.IsCardinal = false
-        data.Switch = true     
+        data.Switch = true
     end
 
     if (sprite:IsPlaying("FloatDown") or sprite:IsPlaying("FloatUp") or sprite:IsPlaying("FloatSide")) and not data.Switch then
         data.Switch = true
-    end 
+    end
 
     if (sprite:IsPlaying("FloatShootDown") or sprite:IsPlaying("FloatShootUp") or sprite:IsPlaying("FloatShootSide")) and data.Switch then
         data.Switch = false
@@ -18,12 +18,10 @@ REWORKEDITEMS:AddCallback(ModCallbacks.MC_FAMILIAR_UPDATE, function(_, familiar)
 end, FamiliarVariant.LIL_LOKI)
 
 ---@param tear EntityTear
-REWORKEDITEMS:AddCallback(ModCallbacks.MC_POST_TEAR_INIT, function(_, tear)
-    if tear.SpawnerEntity and tear.SpawnerType == EntityType.ENTITY_FAMILIAR and tear.SpawnerVariant == FamiliarVariant.LIL_LOKI then
-        local familiar = tear.SpawnerEntity:ToFamiliar()
-        if familiar:GetData().IsCardinal then
-            tear.Velocity = tear.Velocity:Rotated(45)
-        end
-        tear.CollisionDamage = 4
+REWORKEDITEMS:AddCallback(ModCallbacks.MC_POST_FAMILIAR_FIRE_PROJECTILE, function(_, tear)
+    local familiar = tear.SpawnerEntity:ToFamiliar()
+    if familiar:GetData().IsCardinal then
+        tear.Velocity = tear.Velocity:Rotated(45)
     end
-end)
+    tear.CollisionDamage = 4
+end, FamiliarVariant.LIL_LOKI)
