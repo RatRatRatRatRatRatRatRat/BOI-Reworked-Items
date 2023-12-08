cf6ae3745b4a3c9c52cf553fd289ae04d93011e9
Swagpillar <kfeeley20@gmail.com>
2023-12-08 12:28:07 +0000

    aaaaaaaaaa

--- main.lua
+++ main.lua
@@ -59,3 +59,5 @@ REWORKEDITEMS:AddCallback(ModCallbacks.MC_POST_PLAYER_UPDATE, function(_, player
         print(heldEntity.Type.. "." ..heldEntity.Variant.. "." ..heldEntity.SubType)
     end
 end)
+
+--test
