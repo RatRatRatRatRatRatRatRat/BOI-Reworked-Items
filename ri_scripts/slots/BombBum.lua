202b6331f7654da81f53bf8ff7e64dbc27f0f1eb
Swagpillar <kfeeley20@gmail.com>
2024-02-06 23:36:07 +0000

    key and bomb guy

--- ri_scripts/slots/BombBum.lua
+++ ri_scripts/slots/BombBum.lua
@@ -10,17 +10,29 @@ REWORKEDITEMS:AddCallback(ModCallbacks.MC_PRE_SLOT_COLLISION, function(_, slot, 
         local sprite = slot:GetSprite()
         sprite:ReplaceSpritesheet(2, "gfx/items/slots/slot_009_bomb_bum_gold.png", true)
         sprite:Play("PayPrize")
-        slot:SetState(2)
-        slot:SetPrizeType(GoldenPrize)
+        slot:SetState(GoldenPrize)
         return false
     end
 end, SlotVariant.BOMB_BUM)
 
 REWORKEDITEMS:AddCallback(ModCallbacks.MC_POST_SLOT_UPDATE, function(_, slot)
-    if slot:GetPrizeType() ~= GoldenPrize then return end
+    if slot:GetState() ~= GoldenPrize then return end
 
-    if slot:GetState() == 1 then
-        slot:GetSprite():Play("Prize")
-        slot:SetState(2)
+    local sprite = slot:GetSprite()
+    if sprite:IsFinished("PayPrize") then
+        sprite:Play("Prize")
+    end
+
+    if sprite:IsPlaying("Prize") then
+        if sprite:IsEventTriggered("Prize") then
+            local collectible = Game():GetItemPool():GetCollectible(ItemPoolType.POOL_BOMB_BUM, true, slot.DropSeed)
+            local position = Game():GetRoom():FindFreePickupSpawnPosition(slot.Position + Vector(0, 80))
+            slot:GetSprite():Play("Teleport")
+            slot:SetState(4)
+            Isaac.Spawn(EntityType.ENTITY_PICKUP, PickupVariant.PICKUP_COLLECTIBLE, collectible, position, Vector.Zero, slot)    
+        elseif sprite:IsFinished("Prize") then
+            slot:SetState(4)
+            sprite:Play("Teleport")
+        end
     end
 end, SlotVariant.BOMB_BUM)
--- ri_scripts/slots/KeyMaster.lua
+++ ri_scripts/slots/KeyMaster.lua
@@ -10,17 +10,30 @@ REWORKEDITEMS:AddCallback(ModCallbacks.MC_PRE_SLOT_COLLISION, function(_, slot, 
         local sprite = slot:GetSprite()
         sprite:ReplaceSpritesheet(2, "gfx/items/slots/slot_007_key_master_gold.png", true)
         sprite:Play("PayPrize")
-        slot:SetState(2)
-        slot:SetPrizeType(GoldenPrize)
+        slot:SetState(GoldenPrize)
         return false
     end
 end, SlotVariant.KEY_MASTER)
 
+---@param slot EntitySlot
 REWORKEDITEMS:AddCallback(ModCallbacks.MC_POST_SLOT_UPDATE, function(_, slot)
-    if slot:GetPrizeType() ~= GoldenPrize then return end
+    if slot:GetState() ~= GoldenPrize then return end
 
-    if slot:GetState() == 1 then
-        slot:GetSprite():Play("Prize")
-        slot:SetState(2)
+    local sprite = slot:GetSprite()
+    if sprite:IsFinished("PayPrize") then
+        sprite:Play("Prize")
+    end
+
+    if sprite:IsPlaying("Prize") then
+        if sprite:IsEventTriggered("Prize") then
+            local collectible = Game():GetItemPool():GetCollectible(ItemPoolType.POOL_KEY_MASTER, true, slot.DropSeed)
+            local position = Game():GetRoom():FindFreePickupSpawnPosition(slot.Position + Vector(0, 80))
+            slot:GetSprite():Play("Teleport")
+            slot:SetState(4)
+            Isaac.Spawn(EntityType.ENTITY_PICKUP, PickupVariant.PICKUP_COLLECTIBLE, collectible, position, Vector.Zero, slot)    
+        elseif sprite:IsFinished("Prize") then
+            slot:SetState(4)
+            sprite:Play("Teleport")
+        end
     end
 end, SlotVariant.KEY_MASTER)
