f51d22b59f4aeb42dd336245cce28bd084f610e0
Swagpillar <kfeeley20@gmail.com>
2024-05-25 01:39:36 +0100

    scauplar is good now OKAY???

--- ri_scripts/items/rebirth/scapular.lua
+++ ri_scripts/items/rebirth/scapular.lua
@@ -9,16 +9,29 @@ end
 
 mod:AddCallback(ModCallbacks.MC_POST_PEFFECT_UPDATE, mod.BlockScapular)
 
+---@param player EntityPlayer
+function mod:ScapularNewRoom(player)
+    if player:HasCollectible(CollectibleType.COLLECTIBLE_SCAPULAR, false, true) then
+        local effects = player:GetEffects()
+        if not effects:HasNullEffect(NullItemID.ID_HOLY_CARD) then
+            effects:AddCollectibleEffect(CollectibleType.COLLECTIBLE_SCAPULAR)
+        end
+    end
+end
+
+mod:AddCallback(ModCallbacks.MC_POST_PLAYER_NEW_ROOM_TEMP_EFFECTS, mod.ScapularNewRoom)
+
 ---@param entity Entity
 function mod:ScapularTakeDmg(entity)
     local player = entity:ToPlayer()
+    print("A")
 
     if player and player:HasCollectible(CollectibleType.COLLECTIBLE_SCAPULAR, false, true) then
-        if player:GetHearts() + player:GetSoulHearts() + player:GetBoneHearts() <= 1 then
+        if player:GetHearts() + player:GetSoulHearts() + player:GetBoneHearts() <= 2 then
             local effects = player:GetEffects()
-            if not effects:HasCollectibleEffect(CollectibleType.COLLECTIBLE_SCAPULAR) then
-                effects:AddCollectibleEffect(CollectibleType.COLLECTIBLE_HOLY_MANTLE)
-                effects:AddCollectibleEffect(CollectibleType.COLLECTIBLE_SCAPULAR)
+            if effects:HasCollectibleEffect(CollectibleType.COLLECTIBLE_SCAPULAR) then
+                player:UseCard(Card.CARD_HOLY, UseFlag.USE_NOANIM | UseFlag.USE_NOANNOUNCER | UseFlag.USE_NOHUD)
+                effects:RemoveCollectibleEffect(CollectibleType.COLLECTIBLE_SCAPULAR)
             end
         end
     end
